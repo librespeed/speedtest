1dbbbe133344de0fe481cf1fabe5173c13d106e8
Maddie Zhan <maddie.zhan@cynovan.com>
2020-02-24 23:26:58 +0800

    Fix Redact IP Addresses

--- results/telemetry.go
+++ results/telemetry.go
@@ -14,6 +14,7 @@ import (
 	"strings"
 	"time"
 
+	"backend/config"
 	"backend/database"
 	"backend/database/schema"
 
@@ -36,8 +37,6 @@ const (
 )
 
 var (
-	RedactIPAddress bool
-
 	ipv4Regex     = regexp.MustCompile(`(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)`)
 	ipv6Regex     = regexp.MustCompile(`(([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4})?:)?((25[0-5]|(2[0-4]|1?[0-9])?[0-9])\.){3}(25[0-5]|(2[0-4]|1?[0-9])?[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1?[0-9])?[0-9])\.){3}(25[0-5]|(2[0-4]|1?[0-9])?[0-9]))`)
 	hostnameRegex = regexp.MustCompile(`"hostname":"([^\\\\"]|\\\\")*"`)
@@ -155,7 +154,7 @@ func Record(w http.ResponseWriter, r *http.Request) {
 	logs := r.FormValue("log")
 	extra := r.FormValue("extra")
 
-	if RedactIPAddress {
+	if config.LoadedConfig().RedactIP {
 		ipAddr = "0.0.0.0"
 		ipv4Regex.ReplaceAllString(ispInfo, "0.0.0.0")
 		ipv4Regex.ReplaceAllString(logs, "0.0.0.0")
